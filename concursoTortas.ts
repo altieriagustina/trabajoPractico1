3d54971886c44fa6608f3d25c8a451bf01a46316
altieriagustina <altieriagustina@gmail.com>
2024-09-22 13:48:44 -0300

    Actualizacion 22/09

--- concursoTortas.ts
+++ concursoTortas.ts
@@ -1,5 +1,67 @@
+// ### ¡Gran Concurso de Tortas de Tres Arroyos! 🎂
+
+// Te toca ser jurado de un concurso de tortas, y el número de participantes puede variar. 
+// Tu tarea es pedir al usuario cuántos concursantes habrá y luego solicitar las puntuaciones de 
+// `Sabor`, `Presentación` y `Dificultad` para cada torta. Al final, debes determinar qué torta 
+// tiene el mayor puntaje. Y si hay empate? 🤔 En ese caso, informaremos que se produjo un empate 
+// (no es necesario indicar si fueron dos o mas empates ni entre que tortas es el empate, 
+// solo basta con indicar que se produjo empate si al menos existe uno).
+
+// ## Funciones a implementar:
+
+// calcularPuntaje(sabor, presentacion, dificultad):
+// Recibe tres números entre 1 y 5 que representan las puntuaciones de una torta 
+// y devuelve la suma de esos números (el puntaje total del pastel).
+
+// determinarGanador():
+// Utiliza la librería readline-sync para pedir al usuario el número de participantes, 
+// luego solicita las puntuaciones de cada uno de ellos y usa la función calcularPuntaje para 
+// determinar la torta con el mayor puntaje.
+
+// *Si lo consideran necesario, pueden implementar funciones extra.
+
 import * as rs from "readline-sync";
 
 let cantidadParticipantes: number = rs.questionInt("Ingrese cantidad de participantes: ");
+let puntos : number;
+let sabor: number = rs.questionInt("Ingrese puntos obtenidos en sabor entre 1 y 5: ") // Falta rango: sabor >= 1 || sabor <= 5
+let presentacion: number = rs.questionInt("Ingrese puntos obtenidos en presentacion entre 1 y 5: "); // Falta rango: presentacion >= 1 || presentacion <= 5
+let dificultad: number = rs.questionInt("Ingrese puntos obteni dos en dificultad entre 1 y 5: "); // Falta rango: dificultad >= 1 || dificultad <= 5
 
-console.log(cantidadParticipantes);
+//Mostrar ingreso de puntajes tanta cantidad de veces como participantes tenga
+
+let calcularPuntaje = (sabor: number, presentacion: number, dificultad: number): number => {
+    
+    return sabor + presentacion + dificultad;
+}
+
+//Pide puntos para cantidad de participantes OK
+
+for (let i : number = 1; i < cantidadParticipantes; i++) {
+    sabor = rs.questionInt("Ingrese puntos obtenidos en sabor entre 1 y 5: "); 
+     presentacion = rs.questionInt("Ingrese puntos obtenidos en presentacion entre 1 y 5: "); 
+     dificultad = rs.questionInt("Ingrese puntos obteni dos en dificultad entre 1 y 5: "); 
+    sabor++
+    presentacion++
+    dificultad++
+}    
+
+// for (let i : number = 1; i <= cantidadParticipantes; i++) {
+//         contador++
+//         console.log("Participante Nro " + i);
+
+//     }
+        
+//Funcion que calcula TOTAL 
+
+
+
+// let total : number = calcularPuntaje(sabor, presentacion, dificultad);
+
+
+
+
+
+    
+
+
